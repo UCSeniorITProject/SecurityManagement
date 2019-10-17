cb34912ec3611824276777c56e2b5ab1c1f97816
Samuel Curry <currysl@ucmail.uc.edu>
2019-10-17 16:42:36 -0400

    WIP models

--- src/user/UserModel.js
+++ src/user/UserModel.js
@@ -1,2 +1,36 @@
 const SequelizeInstance = require('../dbConnection');
 const Sequelize = require('sequelize');
+const config = require('../../config');
+const activeEnum = require('../constants/activeEnum');
+const userSeedData = require('./userSeedData');
+const bcrypt = require('bcrypt');
+
+const User = SequelizeInstance.define('User', {
+    username: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    phoneNumber: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    instanceMethods: {
+      isValidPassword(password){
+        
+      }
+    }
+  }
+);
