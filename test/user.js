be452e562782305d6c8b01d1f39e6a8be518e0f7
Samuel Curry <currysl@ucmail.uc.edu>
2019-10-26 12:56:40 -0400

    some more user tests

--- test/user.js
+++ test/user.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 const fastify = require('../src/index');
 const userHelpers = require('./helpers/user');
 const fakeUserDetails = userHelpers.createMockUserObject();
+const {signAsync} = require('../src/constants/helpers/jwt');
+const config = require('../config');
 
 describe('UserRole API', async function(){
   let token;
@@ -17,23 +19,18 @@ describe('UserRole API', async function(){
 
   describe('API token validation', async () => {
     it('stops request on no auth token provided', async () => {
-
+      const testRequest = await userHelpers.getUserWithFilter(fastify, {}, '');
+      assert.strictEqual(testRequest.statusCode, 401, 'API let unauthenticated request through');
     });
 
     it('stops a request when an invalid auth token is provided', async () => {
-
-    });
-
-    it('provides a refresh token when an access token is created', () => {
-
-    });
-
-    it('refreshes the token on API request', async () => {
-
+      const testRequest = await userHelpers.getUserWithFilter(fastify, {},  'qweqweqweqqwe');
+      assert.strictEqual(testRequest.statusCode, 401, 'API let invalid token access through');
     });
 
     it('allows a request through when a token is provided', async () => {
-
+      const testRequest = await userHelpers.getUserWithFilter(fastify, {}, token);
+      assert.ok(testRequest.statusCode !== 401 && testRequest.statusCode !== 403, 'Valid acess token was not allowed');
     });
   });
 
@@ -47,16 +44,16 @@ describe('UserRole API', async function(){
 
     it('password gets hashed',  async() => {
       const passwordAfterPost = JSON.parse(apiUser.body).password;
-      assert.notStrictEqual(testUser.password, passwordAfterPost, 'Passwords is not getting hashed');
+      assert.notStrictstrictEqual(testUser.password, passwordAfterPost, 'Passwords is not getting hashed');
     });
 
     it('creates a new user', async () => {
-      assert.equal(apiUser.statusCode, 200, 'User was not succesfully created');
+      assert.strictEqual(apiUser.statusCode, 200, 'User was not succesfully created');
     });
 
     it('should not allow a duplicate user to be  created', async () => {
       const createdUser = await userHelpers.createUser(fastify, testUser);
-      assert.equal(createdUser.statusCode, 500, 'Duplicate user was allowed to be created');
+      assert.strictEqual(createdUser.statusCode, 500, 'Duplicate user was allowed to be created');
     });
   });
 
@@ -84,7 +81,7 @@ describe('UserRole API', async function(){
       delete clonedTestUser.password;
 
       //password gets hashed
-      assert.deepStrictEqual(formattedUpdatedUser, clonedTestUser, 'User is not getting updated');
+      assert.deepStrictstrictEqual(formattedUpdatedUser, clonedTestUser, 'User is not getting updated');
     });
 
     it('password hashes on update', async() => {
@@ -92,14 +89,14 @@ describe('UserRole API', async function(){
       const mockUpdateData = userHelpers.createMockUserObject();
       const updatedUser = await userHelpers.updateUser(fastify, userID, mockUpdateData, token);
       const parsedUpdatedUser = JSON.parse(updatedUser.body).user;
-      assert.notEqual(parsedUpdatedUser.password, mockUpdateData.password, 'Passwords are not being hashed on update')
+      assert.notstrictEqual(parsedUpdatedUser.password, mockUpdateData.password, 'Passwords are not being hashed on update')
     });
 
     it('returns 404 on no rows updated', async () =>{ 
       const userID = 5000000;
       const mockUpdateData = userHelpers.createMockUserObject();
       const updatedUser = await userHelpers.updateUser(fastify, userID, mockUpdateData, token);
-      assert.equal(updatedUser.statusCode, 404, 'Rows updated when 404 should have been thrown');
+      assert.strictEqual(updatedUser.statusCode, 404, 'Rows updated when 404 should have been thrown');
     });
   });
 
@@ -118,8 +115,8 @@ describe('UserRole API', async function(){
         password: testUser.password,
       });
       const parsedToken = JSON.parse(authToken.body).accessToken;
-      assert.equal(authToken.statusCode, 200, 'Login API did not return success');
-      assert.notStrictEqual(parsedToken, undefined, 'Valid login did not provide token');
+      assert.strictEqual(authToken.statusCode, 200, 'Login API did not return success');
+      assert.notStrictstrictEqual(parsedToken, undefined, 'Valid login did not provide token');
     });
 
     it('returns a 401 on invalid login', async () => {
@@ -127,12 +124,21 @@ describe('UserRole API', async function(){
         username: 'qweqwe',
         password: 'qweqweqweqweqqweqwe123123123123123123',
       });
-      assert.equal(authToken.statusCode, 401, 'API did not validate login properly');
+      assert.strictEqual(authToken.statusCode, 401, 'API did not validate login properly');
+    });
+
+    it('provides a refresh token when an access token is created', () => {
+      const authToken = await userHelpers.login(fastify, {
+        username: testUser.username,
+        password: testUser.password,
+      });
+      const tokens = JSON.parse(authToken.body);
+      assert.notEqual(tokens.refreshToken, undefined,  'Refresh token was not granted');
     });
 
     it('returns 500 status code when sent an invalid body', async () => {
       const authToken = await userHelpers.login(fastify, {});
-      assert.equal(authToken.statusCode, 400, 'API schema validation failed');
+      assert.strictEqual(authToken.statusCode, 400, 'API schema validation failed');
     });
   });
 
@@ -147,12 +153,12 @@ describe('UserRole API', async function(){
     it('shows a newly created user in the results', async () => {
       const apiUserID = JSON.parse(apiUser.body).user.id;
       const user = await userHelpers.getUserWithFilter(fastify, {id: apiUserID}, token);
-      assert.equal(JSON.parse(user.body).users[0].id, apiUserID, 'The user was not successfully fetched');
+      assert.strictEqual(JSON.parse(user.body).users[0].id, apiUserID, 'The user was not successfully fetched');
     });
 
     it('shows no results when an invalid query is provided', async () => {
       const user = await userHelpers.getUserWithFilter(fastify, {id: 1290123901, active: 'N', phoneNumber: '5123123123123123123',}, token);
-      assert.equal(JSON.parse(user.body).users.length, 0, 'The API returned results when it should not have');
+      assert.strictEqual(JSON.parse(user.body).users.length, 0, 'The API returned results when it should not have');
     });
   });
 
@@ -166,6 +172,12 @@ describe('UserRole API', async function(){
 
     it('verifies valid token', () => {
       
+    });
+
+    it('does not allowed expired token', () => {
+      const jwtToken = await signAsync({}, config.jwtSecret, {'expiresIn': '-10s'});
+      const testRequest = await userHelpers.getUserWithFilter(fastify, {}, jwtToken);
+      assert.ok(testRequest.statusCode !== 401 && testRequest.statusCode !== 403, 'Expired token was allowed through the API');
     });
 
     it('rejects invalid token', () => {
