9ea3533734569093607ccf1f439e7bc4079e7a49
Samuel Curry <currysl@ucmail.uc.edu>
2019-10-30 11:36:50 -0400

    made swagger config dynamic

--- swagger-config.js
+++ swagger-config.js
@@ -1,4 +1,4 @@
-
+const config = require('./config');
 exports.options = {
   routePrefix: '/documentation',
   exposeRoute: true,
@@ -12,7 +12,7 @@ exports.options = {
       url: 'https://swagger.io',
       description: 'Find more info here'
     },
-    host: 'localhost:3000',
+    host: config.serverHost,
     schemes: ['http'],
     consumes: ['application/json'],
     produces: ['application/json']
